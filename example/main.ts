7cc467dc1ae1774bb9f2f4558d190f0715cba364
JanGordon <timmyjan46@gmail.com>
2023-11-08 22:28:57 +0000

    move example into folder

---
+++ example/main.ts
@@ -0,0 +1,107 @@
+import {html, css, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+import * as HISTORY from "../main";
+
+type dataType = {name: string, age: string}
+const data: dataType = {
+    name: "martini",
+    age: "12"
+}
+
+const session = new HISTORY.Session()
+
+const input = document.getElementById("name")! as HTMLInputElement
+
+
+let a = new HISTORY.SimpleAction({d:data}, (data)=>{
+    data.d.name = "a woof of space"
+    return true
+}, true, "update name")
+
+
+
+session.commitAction([a])
+
+console.log(data)
+console.log("loc", session.currentLocation)
+
+document.getElementById("commit")?.addEventListener("click", ()=>{
+    var inputValue = input.value
+    let a = new HISTORY.SimpleAction({d: data}, (data)=>{
+        data.d.name = inputValue
+        console.log("im in the forward function", inputValue)
+        return true
+    }, true, "update name to" + inputValue)
+    session.commitAction([a])
+    console.log(data, "loc", session.currentLocation)
+
+})
+
+document.getElementById("undo")?.addEventListener("click", ()=>{
+    session.undo()
+    input.value = data.name
+    console.log(data, session, "loc", session.currentLocation)
+
+
+})
+
+document.getElementById("redo")?.addEventListener("click", ()=>{
+    console.log(session.redo())
+    input.value = data.name
+
+    console.log(data, "loc", session.currentLocation)
+
+
+})
+
+
+@customElement("history-tree")
+class HistoryTreeComponent extends LitElement {
+    static styles = css`
+        :host {
+            width: 100%;
+            display: flex;
+        }
+        .HISTORY.Branch {
+            display: flex;
+        }
+        .action.current {
+            color: red;
+        }
+        .HISTORY.Branch-junction {
+            display: flex;
+            flex-direction: column;
+        }
+    `
+
+    @property()
+    session = session
+
+    constructor() {
+        super()
+        this.session.onLocationChange.push(()=>{
+            this.requestUpdate("session")
+        })
+    }
+
+
+    Branch(Branch: HISTORY.Branch) {
+        return html`<div class="HISTORY.Branch">${Branch.map((v)=>Array.isArray(v)
+            ? this.BranchJunction(v)
+            : html `<button class="action ${(this.session.currentAction() == v) ? "current" : ""}">${v.name}</button>`    
+            )}</div>`
+        
+    }
+    BranchJunction(junction: HISTORY.BranchJunction) {
+        return html`
+            <div class="HISTORY.Branch-junction">${junction.map((v)=>this.Branch(v))}</div>
+        `
+    }
+
+    render() {
+        return html`
+            ${this.Branch(this.session.actions)}
+        `
+    }
+}
--- main.ts
+++ main.ts
@@ -1,107 +1,243 @@
-import {html, css, LitElement} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+type ActionResult = boolean
 
-import * as HISTORY from "./history";
-
-type dataType = {name: string, age: string}
-const data: dataType = {
-    name: "martini",
-    age: "12"
+export type Action = {
+    forward: ()=>ActionResult
+    backward: ()=>ActionResult
+    description?: string
+    name?: string
 }
 
-const session = new HISTORY.Session()
-
-const input = document.getElementById("name")! as HTMLInputElement
-
-
-let a = new HISTORY.SimpleAction({d:data}, (data)=>{
-    data.d.name = "a woof of space"
-    return true
-}, true, "update name")
-
-
-
-session.commitAction([a])
-
-console.log(data)
-console.log("loc", session.currentLocation)
-
-document.getElementById("commit")?.addEventListener("click", ()=>{
-    var inputValue = input.value
-    let a = new HISTORY.SimpleAction({d: data}, (data)=>{
-        data.d.name = inputValue
-        console.log("im in the forward function", inputValue)
-        return true
-    }, true, "update name to" + inputValue)
-    session.commitAction([a])
-    console.log(data, "loc", session.currentLocation)
-
-})
-
-document.getElementById("undo")?.addEventListener("click", ()=>{
-    session.undo()
-    input.value = data.name
-    console.log(data, session, "loc", session.currentLocation)
-
-
-})
-
-document.getElementById("redo")?.addEventListener("click", ()=>{
-    console.log(session.redo())
-    input.value = data.name
-
-    console.log(data, "loc", session.currentLocation)
-
-
-})
-
-
-@customElement("history-tree")
-class HistoryTreeComponent extends LitElement {
-    static styles = css`
-        :host {
-            width: 100%;
-            display: flex;
-        }
-        .HISTORY.Branch {
-            display: flex;
-        }
-        .action.current {
-            color: red;
-        }
-        .HISTORY.Branch-junction {
-            display: flex;
-            flex-direction: column;
-        }
-    `
-
-    @property()
-    session = session
-
-    constructor() {
-        super()
-        this.session.onLocationChange.push(()=>{
-            this.requestUpdate("session")
-        })
+export class SimpleAction<d extends object> {
+    name: string
+    description: string
+    forwardFunction: (data: d)=>ActionResult
+    forward() {
+        return this.forwardFunction(this.proxiedData)
     }
+    backward() {
+        if (this.hasMultipleData) {
+            for (var [key, val] of Object.entries(this.fieldStorage)) {
+                if (typeof val == "object") {
+                    for (let [k, v] of Object.entries(val as any)) {
+                        this.data[key][k] = v
+                    }
+                }
+                
+            }
+        } else {
+            for (let [k, v] of Object.entries(this.fieldStorage)) {
+                this.data[k] = v
+            }
+        }
+        return true
+    }
+    data: d
+    proxiedData: d
+    fieldStorage = {}
+    hasMultipleData?: boolean
+    constructor(data: d, forwardFunction: (data: d)=>ActionResult, hasMultipleData?: boolean, name?: string, description?: string) {
+        this.name = name
+        this.description = description
+        this.forwardFunction = forwardFunction
+        this.hasMultipleData = hasMultipleData
+        this.data = data
+        //create proxy
+        
+        if (hasMultipleData) {
+            var tempObject: any = {}
+            for (let [k, v] of Object.entries(data)) {
+                var setFunction = (target: d, prop: string, reciever: any)=>{
+                    if (target[prop] != reciever) {
+                        this.fieldStorage[k] = {}
+                        this.fieldStorage[k][prop] = target[prop]
+                        target[prop] = reciever
+                    }
+                    console.log(prop, "changed")
+                    return true
+                }
+                var handler: ProxyHandler<any> = {
+                    get(target, prop, reciever) {
+                        return target[prop]
+                    },
+                    set: setFunction
+                }
+                tempObject[k] = new Proxy(v, handler)
+            }
 
+            this.proxiedData = tempObject
+        } else {
+            var setFunction = (target: d, prop: string, reciever: any)=>{
+                if (target[prop] != reciever) {
+                    this.fieldStorage[prop] = target[prop]
+                    target[prop] = reciever
+                }
+                console.log(prop, "changed")
 
-    Branch(Branch: HISTORY.Branch) {
-        return html`<div class="HISTORY.Branch">${Branch.map((v)=>Array.isArray(v)
-            ? this.BranchJunction(v)
-            : html `<button class="action ${(this.session.currentAction() == v) ? "current" : ""}">${v.name}</button>`    
-            )}</div>`
+    
+                return true
+            }
+            var handler: ProxyHandler<any> = {
+                get(target, prop, reciever) {
+                    return target[prop]
+                },
+                set: setFunction
+            }
+            this.proxiedData = new Proxy(data, handler)
+        }
+        
+        
         
     }
-    BranchJunction(junction: HISTORY.BranchJunction) {
-        return html`
-            <div class="HISTORY.Branch-junction">${junction.map((v)=>this.Branch(v))}</div>
-        `
+}
+
+
+
+export type Branch = (Action | BranchJunction)[]
+export type BranchJunction = Branch[]
+
+
+export class Session {
+    onLocationChange: (()=>void)[] = []
+    changedLocation() {
+        for (let i of this.onLocationChange) {
+            i()
+        }
+    }
+    currentBranch() {
+        console.log(this.currentLocation)
+
+        var getItemAt = (branch: Branch, currentLocationIndex: number)=>{
+            var d = branch[this.currentLocation[currentLocationIndex]]
+            if (Array.isArray(d)) {
+                //its a junction
+                var nextBranch = d[this.currentLocation[currentLocationIndex+1]]
+                return getItemAt(nextBranch, currentLocationIndex+1)
+            } else {
+                // its an action
+                return branch
+            }
+        }
+        return getItemAt(this.actions, 0)
+    }
+    currentAction() {
+        return this.currentBranch()[this.currentLocation[this.currentLocation.length-1]]
+    }
+    actions: Branch = [{forward:()=>true, backward: ()=>true}]
+    readonly currentLocation = [0]// cange this
+    // currentBranch: (Action | BranchJunction)[] = this.actions
+    commitAction(actions: Action[]) {
+        
+
+        let currentBranch = this.currentBranch()
+        // check if we are at end of branch because if not, we need to mkae a new branch junction
+        if (currentBranch.length-1 == this.currentLocation[this.currentLocation.length-1]) {
+            this.currentLocation[this.currentLocation.length-1]+=actions.length
+            for (let i of actions) {
+                i.forward()
+                currentBranch.push(i)
+            }
+            this.changedLocation()
+        } else {
+            console.log("creating a new branhc at position", )
+            // get all actions / branches after the new branchjunction
+            let newBranchJunctionLocation = currentBranch.indexOf(this.currentAction())
+            let oldBranch = currentBranch.slice(newBranchJunctionLocation)
+            currentBranch.splice(newBranchJunctionLocation)
+            let newBranch: Branch = []
+            for (let i of actions) {
+                i.forward()
+                newBranch.push(i)
+            }
+            currentBranch[newBranchJunctionLocation] = [oldBranch, newBranch]
+            this.currentLocation.push(1, 0)// take the 2nd exit on the branch,
+            // the 0 shows that it is the 1st index on that branch
+            this.changedLocation()
+        }
+    }
+    undo() {
+        console.log(this.currentLocation)
+        var decrementLastIndex = (onBranchLocation: boolean)=>{
+            if (this.currentLocation.length > 0) {
+
+                
+                // gets the last entry in the location and checks if it can be decremented without going below 0
+                if (this.currentLocation[this.currentLocation.length-1]-1 < 0) {
+                    // this is the first element in the branch so we'll have to decrement the previous value
+                    // therefore we need to remove it
+                    var currentBranch = this.currentBranch()
+
+                    // we can safely ignore this because it will always be action because there is never a branchJunction on the end of the currentLocation
+                    //@ts-ignore
+                    currentBranch[this.currentLocation[this.currentLocation.length-1]].backward()
+                    this.currentLocation.pop()
+                    // because before every number that defines where we are in a branch there is a 
+                    // number that defines which route to take on the branch junction,
+                    // we can ignore the value directly before and move on to the one before and decrement that
+                    // (actually we need to remove it first)
+                    this.currentLocation.pop()
+                    // this.currentLocation[this.currentLocation.length-1]--
+                    // we then decrement the next value
+                    // but only if it is above 0
+                    decrementLastIndex(true)
+                        
+                } else {
+
+                    // we finally found the branch that has at least 1 actions in it
+                    if (onBranchLocation) {
+                        // a branch
+                        this.currentLocation[this.currentLocation.length-1]--
+                        //@ts-ignore
+
+                    } else {
+                        // we can safely ignore this because it will always be action because there is never a branchJunction on the end of the currentLocation
+                        console.log(this.currentLocation, this.actions, this.currentAction())
+                        //@ts-ignore
+                        this.currentAction().backward()
+                        this.currentLocation[this.currentLocation.length-1]--
+
+                    }
+                    
+                }
+            } else {
+                // alert("youve reached teh dstart")
+            }
+        }
+        
+        decrementLastIndex(false)
+        this.changedLocation()
+
     }
 
-    render() {
-        return html`
-            ${this.Branch(this.session.actions)}
-        `
+    redo() {
+        let currentBranch = this.currentBranch()
+        if (this.currentLocation[this.currentLocation.length-1] == currentBranch.length) {
+            // we are at end of branch
+            return false
+        } else {
+            // we know there are still items in branch
+            console.log(currentBranch)
+            // currently unsafe if it is a branch junction
+            var traverseJunctions = (action: Action | BranchJunction)=>{
+                if (Array.isArray(action)) {
+                    // we are at a branch junction 
+                    // ask which route to take
+                    let route = prompt("there is a diverging junction here, pick which route to take, (0-"+(action.length-1)+"): ")
+                    this.currentLocation.push(parseInt(route ? route : "0"))
+                    this.currentLocation.push(0) // first action of the branch
+                    
+                    
+                    return traverseJunctions(this.currentBranch()[0])
+                } else {
+                    console.log("going forward on", this.currentLocation)
+                    action.forward()
+                    return true
+                }
+            }
+            this.currentLocation[this.currentLocation.length-1]++
+            this.changedLocation()
+
+            return traverseJunctions(currentBranch[this.currentLocation[this.currentLocation.length-1]])
+        }
     }
+    
 }
